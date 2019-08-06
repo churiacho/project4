1948ac578ee53d43590813560ec72d779bb5a19f
Julia Jo-Work <julia.ohyoon@gmail.com>
2019-08-06 16:57:30 -0400

    - Changed homepage for Andrii style points - removed fontawesome dependancy

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -1,48 +1,45 @@
 import React, { Component } from 'react';
-import WorkCard from '../Components/WorkCard.js';
 import './Home.css';
-import Scura from '../Components/scura.png';
-import ScuraAbout from '../Components/scuraAbout.png';
-import ScuraForm from '../Components/scuraForm.png';
-import ScuraProps from '../Components/scuraProps.png';
-import Boff from '../Components/boff.png';
-import BoffAbout from '../Components/boffAbout.png';
-import BoffFilm from '../Components/boffFilm.png';
-import BoffForm from '../Components/boffFooter.png';
+import { Jumbotron, Button } from 'react-bootstrap';
+
 
 
 
 
 
 export class Home extends Component {
+
+
   render() {
     return (
       <div>
-        <WorkCard 
-          slide1={Scura}
-          firstSlide="Scura Homepage"
-          slide2={ScuraAbout}
-          secondSlide="Scura About Page"
-          slide3={ScuraForm}
-          thirdSlide="Scura Contact Form"
-          slide4={ScuraProps}
-          fourthSlide="Scura Properties Page"
-        />
-        <WorkCard 
-          slide1={Boff}
-          firstSlide="Brooklyn Outdoor Film Festival Homepage"
-          slide2={BoffAbout}
-          secondSlide="About the organizers"
-          slide3={BoffFilm}
-          thirdSlide="Posts page"
-          slide4={BoffForm}
-          fourthSlide="Pre-registration form embedded in the footer"
-        />
+        <Jumbotron fluid className="homeJumbo">
+          <h1>This is where the intro will go</h1>
+          <Button variant="outline-light">
+            <h1>
+            <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+ width="20pt" height="20pt" viewBox="0 0 1280.000000 1172.000000"
+ preserveAspectRatio="xMidYMid meet">
+<metadata>
+Created by potrace 1.15, written by Peter Selinger 2001-2017
+</metadata>
+<g transform="translate(0.000000,1172.000000) scale(0.100000,-0.100000)"
+fill="white" stroke="none">
+<path d="M5820 6563 l0 -4578 -2413 2413 -2412 2412 -413 -413 -412 -412 2993
+-2993 2992 -2992 245 0 245 0 2992 2992 2993 2993 -412 412 -413 413 -2412
+-2412 -2413 -2413 0 4578 0 4577 -580 0 -580 0 0 -4577z"/>
+</g>
+</svg>
+
+            </h1> 
+          </Button>
+        </Jumbotron>
 
       </div>
     )
   }
 }
+
 
 
 
