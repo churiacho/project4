221d8a6bb9d3817cecd55c2fca1cfe39a504169e
Julia Jo-Work <julia.ohyoon@gmail.com>
2019-08-14 14:36:06 -0400

    added profile pic to about page created boff page

---
+++ src/Containers/About/Bio.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Image, Card, Jumbotron, Container, Row, Col } from 'react-bootstrap';
+import Pic from './pic.jpg';
+import './Bio.css';
+
+
+function Bio() {
+  return (
+    <Container id="bio">
+      <Row className="justify-content-center">
+        <Col xs={3}>
+          <Image src={Pic} rounded id="pic"/>        
+        </Col>
+        <Col sm={12} md={6}>
+        <Card>
+          <Card.Header>
+            Julia Jo: Full Stack Developer
+          </Card.Header>
+          <Card.Body>
+            My Bio
+          </Card.Body>
+          </Card>      
+        </Col>          
+  
+        </Row>
+
+      <Row className="justify-content-center">
+        <Jumbotron>Resume probably</Jumbotron>
+      </Row>
+
+    </Container>
+  )
+}
+
+export default Bio;
---
+++ src/Containers/Boff/Boff.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Boff extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Brooklyn Outdoor Film Festival</h1>
+      </div>
+    )
+  }
+}
+
+export default Boff;
