8b8f55bb85c7411b3adb509de03d07a4607bd5be
Julia Jo-Work <julia.ohyoon@gmail.com>
2019-08-15 16:49:01 -0400

    Boff page created

--- src/Containers/Boff/Boff.js
+++ src/Containers/Boff/Boff.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import { Container, Row, Col, Image, Card } from 'react-bootstrap';
+import BoffBg from './boffbg.jpg';
+import './Boff.css';
+
 
 export class Boff extends Component {
   render() {
     return (
-      <div>
-        <h1>Brooklyn Outdoor Film Festival</h1>
+      <div className="boff">
+        <header>
+          <h1 className="display-4">BrooklynOFF.com</h1>
+        </header>
+        <Container fluid>
+          <Row>
+            <Card className="bg-dark text-white">
+
+            </Card>
+          </Row>
+          <Row className="justify-content-center">
+            <Col xs={10}>
+              <Image src={BoffBg} responsive fluid/>                  
+            </Col>
+
+          </Row>
+        </Container>
+    
+
       </div>
     )
   }
