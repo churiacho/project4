dc7923677606d4b8d46dff630e0b0f0a4c2dc361
Julia Jo-Work <julia.ohyoon@gmail.com>
2019-08-19 10:51:40 -0400

    Updates

---
+++ precache-manifest.d8a4e55749d2e04df1d98f0e22cac3ef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eec6fdec39a8fd1b41d569a38de3837",
+    "url": "/project4/index.html"
+  },
+  {
+    "revision": "8d75ad2e5cf08fd70e00",
+    "url": "/project4/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b883a07a8c67d25a3e63",
+    "url": "/project4/static/css/main.d19b0b83.chunk.css"
+  },
+  {
+    "revision": "8d75ad2e5cf08fd70e00",
+    "url": "/project4/static/js/2.c7115deb.chunk.js"
+  },
+  {
+    "revision": "b883a07a8c67d25a3e63",
+    "url": "/project4/static/js/main.2dba3c89.chunk.js"
+  },
+  {
+    "revision": "122b68e21a3b010d0bf0",
+    "url": "/project4/static/js/runtime~main.cbf94597.js"
+  },
+  {
+    "revision": "9ce5e6c4b57a23a16179b0234d9d0688",
+    "url": "/project4/static/media/boffbg.9ce5e6c4.jpg"
+  },
+  {
+    "revision": "90908344aa7b9200010f52b855a61763",
+    "url": "/project4/static/media/motherbg.90908344.jpg"
+  },
+  {
+    "revision": "9759a49203ce36205262172b0c2fe36c",
+    "url": "/project4/static/media/pic.9759a492.jpg"
+  },
+  {
+    "revision": "36b44ac7b81989ff047304429667cb7b",
+    "url": "/project4/static/media/scurabg.36b44ac7.png"
+  }
+]);
